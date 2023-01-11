e5b5d1559b302ac60b00914f27ac250816fac2d7
Simon <simmoe@gmail.com>
2023-01-11 13:29:47 +0100

    updates

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script>
+	//data er det samlede array med ALLE fangerne 
 	let data = []
 	let dataCopy = []
 	fetch('./deathrow.json')
@@ -8,16 +9,39 @@
 				data = json
 				dataCopy = data
 			})
+			let searchName = ''
+			const filterByName = () => {
+				console.log(searchName)
+				dataCopy = data.filter( p => (p["First Name"] + ' ' + p["Last Name"]).toLowerCase().includes(searchName.toLowerCase()))
+			}
+			let race 
+			const filterByRace = () => {
+				if( race == '*') { dataCopy = data; return }
+				dataCopy = data.filter( p => p["Race"].toLowerCase() == race )
+			}
 </script>
 
 <main>
 	<div class="header">
 		<h1>Den sorteste mørkeste hjemmeside</h1>
 	</div>
+	<div class="filter">
+		<p>search by name</p>
+		<input type="text" bind:value={searchName} on:input={filterByName}>
+		<p>filter by what the dataset calls race (you racist bitch)</p>
+		<select bind:value={race} on:change={filterByRace}>
+			<option value="*">please select</option>			
+			<option value="black">black</option>			
+			<option value="white">white</option>			
+			<option value="hispanic">hispanic</option>			
+		</select>
+	</div>
 	<div class="prisoners">
 		{#each dataCopy as prisoner}
 			 <div class="prisoner">
 				<h2>{ prisoner["First Name"] } { prisoner["Last Name"] }</h2>
+				<div>{ prisoner["Age at Execution"] }</div>
+				<div class="statement">{ prisoner["Last Statement"] }</div>
 			 </div>			 
 		{/each}
 	</div>
@@ -33,11 +57,17 @@
 		display:grid;
 		place-items: center;
 	}
+	.filter{
+		display:grid;
+		grid-template-columns: 2fr 5fr;
+		padding:.4rem;
+	}
 	h1{
 		font-weight:100;
 		text-transform:uppercase;
 	}
 	.prisoners{
+		padding-top:1rem;
 		display:grid;
 		grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
 		gap:.4rem;
@@ -47,5 +77,11 @@
 		background-color:darkgray;
 		color:#eee;
 		padding:1rem;
+		transition: all .5s ease-in-out;
+	}
+	.show{
+		position:fixed;
+		width:100vw;
+		height:100vh;
 	}
 </style>
