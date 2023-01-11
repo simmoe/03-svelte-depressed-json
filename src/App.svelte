7f2aad64d374d445bf70ef5863b2544d37413ce2
Simon <simmoe@gmail.com>
2023-01-11 11:32:37 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,51 @@
+<script>
+	let data = []
+	let dataCopy = []
+	fetch('./deathrow.json')
+		.then(res => res.json())
+			.then(json => {
+				console.log(json)
+				data = json
+				dataCopy = data
+			})
+</script>
+
+<main>
+	<div class="header">
+		<h1>Den sorteste mørkeste hjemmeside</h1>
+	</div>
+	<div class="prisoners">
+		{#each dataCopy as prisoner}
+			 <div class="prisoner">
+				<h2>{ prisoner["First Name"] } { prisoner["Last Name"] }</h2>
+			 </div>			 
+		{/each}
+	</div>
+</main>
+
+<style>
+	:global(body){
+		background-color: black;
+		color:whitesmoke;
+	}
+	.header{
+		border:1px solid white;
+		display:grid;
+		place-items: center;
+	}
+	h1{
+		font-weight:100;
+		text-transform:uppercase;
+	}
+	.prisoners{
+		display:grid;
+		grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+		gap:.4rem;
+	}
+	.prisoner{
+		border:1px solid whitesmoke;
+		background-color:darkgray;
+		color:#eee;
+		padding:1rem;
+	}
+</style>
